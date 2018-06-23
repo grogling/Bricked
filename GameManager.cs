e8960aa987643444d58e9c4702fc7ac80080de7c
grogling <40528780+grogling@users.noreply.github.com>
2018-06-23 15:28:58 -0700

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject[] Units = new GameObject[4];
+	public GameObject ActiveUnit;
+	private int _activeUnitIndex;
+
+	private float _currentInterval;
+
+	// Use this for initialization
+	void Start () {
+		ActiveUnit = Units [0];
+	}
+
+	void Update () {
+
+		_currentInterval += Time.deltaTime;
+		if (_currentInterval >= 1) {
+			_activeUnitIndex++;
+			if (_activeUnitIndex == this.Units.Length){
+				_activeUnitIndex = 0;
+			}
+
+			ActiveUnit = Units[_activeUnitIndex];
+			_currentInterval = 0;
+
+			// pop the unit when selected
+			ActiveUnit.transform.DOScale(2.5f, 0.1f).OnComplete(() => {
+				ActiveUnit.transform.localScale = new Vector3(1, 1, 1);
+			});
+		}
+	}
+}
---
+++ movement.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class movement : MonoBehaviour {
+
+    private float unitspeed = 5f;
+	private Vector3 unitposition= new Vector3(0,0,0);
+	private bool canMove = true , moving=false;
+	private float unitfreq =0f;
+
+	public bool IsUnit1;
+	public bool IsUnit2;
+	public bool IsUnit3;
+	public bool IsUnit4;
+	public float turntime=2f;
+
+	public GameObject[] Units = new GameObject[4];
+
+	// Update is called once per frame
+	void Update ()
+    {
+	unitfreq += Time.deltaTime;
+
+	if (IsUnit1 == true) 
+	{
+		if (unitfreq >0 && unitfreq < turntime)
+		{
+
+			MoveExecution ();			
+		}
+	}
+	if (IsUnit2 == true) 
+	{
+		if (unitfreq > turntime && unitfreq < 2*turntime)
+		{
+			MoveExecution ();
+
+		}
+	}
+	if (IsUnit3 == true) 
+	{
+		if (unitfreq >2*turntime && unitfreq <3*turntime)
+		{
+			MoveExecution ();
+				
+		}
+	}
+	if (IsUnit4 == true) 
+	{
+		if (unitfreq >3*turntime && unitfreq <4*turntime)
+		{
+			MoveExecution ();
+				
+		}
+	}
+	if (unitfreq >4*turntime)
+	{
+		unitfreq =0;
+	}
+
+	}
+		 //transform.position= Vector3.MoveTowards(transform.position,unitposition,Time.deltaTime*unitspeed);
+        //playerposition = new Vector3(0f, Mathf.Clamp(ypos, -7, 9), 0);
+        //gameObject.transform.position = playerposition;
+
+	private void Movefun() 
+	{
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{ 
+			canMove = false;
+			moving = true;
+			unitposition += Vector3.up;
+		} else if (Input.GetKey (KeyCode.DownArrow)) 
+		{ 		 
+			canMove = false;
+			moving = true;
+			unitposition += Vector3.down;
+		} else if (Input.GetKey (KeyCode.RightArrow)) 
+		{ 		 
+			canMove = false;
+			moving = true;
+			unitposition += Vector3.right;
+		}
+		else if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+		 
+			canMove = false;
+			moving = true;
+			unitposition+=Vector3.left;
+		}
+
+	}
+	private void MoveExecution()
+	{
+
+
+		if (canMove) 
+		{
+			unitposition = transform.position;
+			Movefun ();
+			
+		}
+		if (moving) 
+		{
+			if (transform.position==unitposition)
+			{
+				moving=false;
+				canMove= true;			
+				Movefun ();
+			}
+			transform.position = Vector3.MoveTowards (transform.position, unitposition, Time.deltaTime*unitspeed);
+		}
+	}
+}
