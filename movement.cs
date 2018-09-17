21717ae8db6820a3059d8275815253a6ddf7e2d4
grogling <40528780+grogling@users.noreply.github.com>
2018-09-16 20:25:56 -0500

    Add files via upload

--- movement.cs
+++ movement.cs
@@ -4,65 +4,25 @@ using DG.Tweening;
 
 public class movement : MonoBehaviour {
 
-    private float unitspeed = 5f;
+    private float unitspeed = 6f;
 	private Vector3 unitposition= new Vector3(0,0,0);
 	private bool canMove = true , moving=false;
-	private float unitfreq =0f;
+    public GameObject AU;
+   
 
-	public bool IsUnit1;
-	public bool IsUnit2;
-	public bool IsUnit3;
-	public bool IsUnit4;
-	public float turntime=2f;
-
-	public GameObject[] Units = new GameObject[4];
-
-	// Update is called once per frame
-	void Update ()
+    void Update ()
     {
-	unitfreq += Time.deltaTime;
+        AU = GameManager.Instance.ActiveUnit;
 
-	if (IsUnit1 == true) 
-	{
-		if (unitfreq >0 && unitfreq < turntime)
-		{
+        if (AU == gameObject)
+        {
+            MoveExecution();
+        }
 
-			MoveExecution ();			
-		}
-	}
-	if (IsUnit2 == true) 
-	{
-		if (unitfreq > turntime && unitfreq < 2*turntime)
-		{
-			MoveExecution ();
 
-		}
-	}
-	if (IsUnit3 == true) 
-	{
-		if (unitfreq >2*turntime && unitfreq <3*turntime)
-		{
-			MoveExecution ();
-				
-		}
-	}
-	if (IsUnit4 == true) 
-	{
-		if (unitfreq >3*turntime && unitfreq <4*turntime)
-		{
-			MoveExecution ();
-				
-		}
-	}
-	if (unitfreq >4*turntime)
-	{
-		unitfreq =0;
-	}
-
-	}
-		 //transform.position= Vector3.MoveTowards(transform.position,unitposition,Time.deltaTime*unitspeed);
-        //playerposition = new Vector3(0f, Mathf.Clamp(ypos, -7, 9), 0);
-        //gameObject.transform.position = playerposition;
+    }
+		
+    
 
 	private void Movefun() 
 	{
@@ -91,10 +51,9 @@ public class movement : MonoBehaviour {
 		}
 
 	}
-	private void MoveExecution()
+	public void MoveExecution()
 	{
-
-
+        
 		if (canMove) 
 		{
 			unitposition = transform.position;
