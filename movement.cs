cd739a98e732e8d10709f42e0ddb248d794e2db5
grogling <40528780+grogling@users.noreply.github.com>
2020-05-21 00:52:15 -0500

    Add files via upload

--- movement.cs
+++ movement.cs
@@ -1,19 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 using DG.Tweening;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System;
 
 public class movement : MonoBehaviour {
 
-    private float unitspeed = 6f;
-	private Vector3 unitposition= new Vector3(0,0,0);
+    private float unitspeed = 15f;
+    private Vector3 unitposition= new Vector3(0,0,0);
 	private bool canMove = true , moving=false;
     public GameObject AU;
-   
+    public float movelength = 2f; // how far the unit moves
+    private float cooldowntime =1f; // cooldown on moving
+    private float nextmovetime = 0f; // the next time you are able to move
 
+    
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
     void Update ()
     {
         AU = GameManager.Instance.ActiveUnit;
-
+        
         if (AU == gameObject)
         {
             MoveExecution();
@@ -21,42 +35,86 @@ public class movement : MonoBehaviour {
 
 
     }
-		
-    
 
-	private void Movefun() 
+    // This function translates directional keys into movement
+    private void Movefun() 
 	{
-		if (Input.GetKey (KeyCode.UpArrow)) 
-		{ 
-			canMove = false;
-			moving = true;
-			unitposition += Vector3.up;
-		} else if (Input.GetKey (KeyCode.DownArrow)) 
-		{ 		 
-			canMove = false;
-			moving = true;
-			unitposition += Vector3.down;
-		} else if (Input.GetKey (KeyCode.RightArrow)) 
-		{ 		 
-			canMove = false;
-			moving = true;
-			unitposition += Vector3.right;
-		}
-		else if (Input.GetKey (KeyCode.LeftArrow)) 
-		{
-		 
-			canMove = false;
-			moving = true;
-			unitposition+=Vector3.left;
-		}
-
+        if (Time.time > nextmovetime)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 1f, 1 << 8);
+                if (hit.collider != null)
+                {
+                    canMove = true;
+                    moving = false;
+                }
+                else
+                {
+                    canMove = false;
+                    moving = true;
+                    unitposition += Vector3.up * movelength; // this is the position the unit will move to when you hit the directional key
+                    nextmovetime = Time.time + cooldowntime; // this let's you only input one movement command per cycle
+                }
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, 1 << 8);
+                if (hit.collider != null)
+                {
+                    canMove = true;
+                    moving = false;
+                }
+                else
+                {
+                    canMove = false;
+                    moving = true;
+                    unitposition += Vector3.down * movelength;
+                    nextmovetime = Time.time + cooldowntime;
+                }
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 1f, 1 << 8);
+                if (hit.collider != null)
+                {
+                    canMove = true;
+                    moving = false;
+                }
+                else
+                {
+                    canMove = false;
+                    moving = true;
+                    unitposition += Vector3.right * movelength;
+                    nextmovetime = Time.time + cooldowntime;
+                }
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, 1f, 1 << 8);
+                if (hit.collider != null)
+                {
+                    canMove = true;
+                    moving = false;
+                }
+                else
+                {
+                    canMove = false;
+                    moving = true;
+                    unitposition += Vector3.left * movelength;
+                    nextmovetime = Time.time + cooldowntime;
+                }
+            }
+        }
 	}
-	public void MoveExecution()
+    // This is the actual execution of the move function
+	public void MoveExecution() 
 	{
         
 		if (canMove) 
 		{
-			unitposition = transform.position;
+			
+            unitposition = transform.position;
 			Movefun ();
 			
 		}
@@ -69,6 +127,7 @@ public class movement : MonoBehaviour {
 				Movefun ();
 			}
 			transform.position = Vector3.MoveTowards (transform.position, unitposition, Time.deltaTime*unitspeed);
-		}
+            
+        }
 	}
 }
