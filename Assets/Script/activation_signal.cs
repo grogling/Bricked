b47c4e836835f01fdaae0e1e01138132fdd17e9c
Hamza Shaikh <53708485+rewazzu@users.noreply.github.com>
2021-07-02 19:08:01 -0700

    fixed signal distance bug

--- Assets/Script/activation_signal.cs
+++ Assets/Script/activation_signal.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class activation_signal : MonoBehaviour {
 
-    private float velx = 0;
-    private float vely = 0;
-    private Vector3 activeunit_POS = new Vector3(0, 0, 0);
-    //private float _magnitude;
-   
-
-    Rigidbody2D rb;
- 
-
-    public GameObject AU;
+  private float velx = 0;
+  private float vely = 0;
+  private Vector3 activeunit_POS = new Vector3(0, 0, 0);
+  //private float _magnitude;
+  Rigidbody2D rb;
+  public GameObject AU;
 
 	// Use this for initialization
 	void Start ()
@@ -23,20 +19,17 @@ public class activation_signal : MonoBehaviour {
         AU = GameManager.Instance.NextUnit; //instaniate active game object
         activeunit_POS = AU.transform.position; //get active game object position
 // old way
-        velx = activeunit_POS.x - transform.position.x;
-        vely = activeunit_POS.y - transform.position.y;
+        velx = (activeunit_POS.x - transform.position.x)*2;
+        vely = (activeunit_POS.y - transform.position.y)*2;
 
         // end old way
 
        // _magnitude = Mathf.Sqrt(Mathf.Pow((AU.transform.position.x - transform.position.x),2) + Mathf.Pow((AU.transform.position.y - transform.position.y),2));
-        
-	}
-	
+    }
+
 	// Update is called once per frame
 	void Update ()
     {
-        rb.velocity = new Vector3 (velx*2, vely*2, 0); // takes 2 seconds to activate each unit
-        
-        
+        rb.velocity = new Vector3 (velx, vely, 0); // takes 2 seconds to activate each unit
     }
 }
